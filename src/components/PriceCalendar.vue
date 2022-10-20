8dbb47da84e14f4fbfa177e33563da02bb86e84a
Kevin Alix <kevinalix19890903@gmail.com>
2022-10-20 23:48:22 +0800

    Update Styles for calendar

--- src/components/PriceCalendar.vue
+++ src/components/PriceCalendar.vue
@@ -1,5 +1,5 @@
 <template>
-  <VCalendar />
+  <VCalendar :attributes="attrs" />
 </template>
 
 <script>
@@ -7,6 +7,33 @@ import VCalendar from 'v-calendar/lib/components/calendar.umd'
 
 export default {
   name: 'PriceCalendar',
+  data() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    return {
+      attrs: [
+        {
+          key: 'today',
+          highlight: {
+            color: 'red',
+            fillMode: 'solid',
+            // contentClass: 'italic',
+          },
+          dates: new Date(year, month, day),
+        },
+        {
+          highlight: {
+            start: { fillMode: 'outline' },
+            base: { fillMode: 'light' },
+            end: { fillMode: 'outline' },
+          },
+          dates: { start: new Date(year, month, 14), end: new Date(year, month, 30) },
+        },
+      ],
+    };
+  },
   components: {
     VCalendar
   },
@@ -17,19 +44,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .vc-container {
+    /* --text-sm: 18px; */
+  }
+  .vc-day-content {
+    width: 42px !important;
+    height: 42px !important;
+  }
+  .vc-highlight {
+    width: 100% !important;
+    height: 100% !important;
+  }
 </style>
