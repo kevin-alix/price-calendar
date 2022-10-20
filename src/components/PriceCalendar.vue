6d23a2b4d4e617be4de75b4cce4a05b6c4bc3b60
Kevin Alix <kevinalix19890903@gmail.com>
2022-10-20 22:21:16 +0800

    Added PriceCalendar component

---
+++ src/components/PriceCalendar.vue
@@ -0,0 +1,35 @@
+<template>
+  <VCalendar />
+</template>
+
+<script>
+import VCalendar from 'v-calendar/lib/components/calendar.umd'
+
+export default {
+  name: 'PriceCalendar',
+  components: {
+    VCalendar
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
