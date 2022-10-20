013e19cd3a45308d7f74b58eb02949b1b785e9cf
Kevin Alix <kevinalix19890903@gmail.com>
2022-10-21 02:52:46 +0800

    Today highlight implemented

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <PriceCalendar msg="Welcome to Your Vue.js App"/>
+    <PriceCalendar :prices="prices"/>
   </div>
 </template>
 
@@ -11,7 +11,85 @@ export default {
   name: 'App',
   components: {
     PriceCalendar
-  }
+  },
+  data() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+
+    return {
+      prices: [
+        {
+          date: new Date(year, month, '14'),
+          price: 'RM 129',
+        },
+        {
+          date: new Date(year, month, '15'),
+          price: 'RM 899',
+        },
+        {
+          date: new Date(year, month, '16'),
+          price: 'RM 39',
+        },
+        {
+          date: new Date(year, month, '17'),
+          price: 'RM 65',
+        },
+        {
+          date: new Date(year, month, '18'),
+          price: 'RM 79',
+        },
+        {
+          date: new Date(year, month, '19'),
+          price: 'RM 199',
+        },
+        {
+          date: new Date(year, month, '20'),
+          price: 'RM 799',
+        },
+        {
+          date: new Date(year, month, '21'),
+          price: 'RM 679',
+        },
+        {
+          date: new Date(year, month, '22'),
+          price: 'RM 69',
+        },
+        {
+          date: new Date(year, month, '23'),
+          price: 'RM 599',
+        },
+        {
+          date: new Date(year, month, '24'),
+          price: 'RM 199',
+        },
+        {
+          date: new Date(year, month, '25'),
+          price: 'RM 299',
+        },
+        {
+          date: new Date(year, month, '26'),
+          price: 'RM 79',
+        },
+        {
+          date: new Date(year, month, '27'),
+          price: 'RM 199',
+        },
+        {
+          date: new Date(year, month, '28'),
+          price: 'RM 99',
+        },
+        {
+          date: new Date(year, month, '29'),
+          price: 'RM 299',
+        },
+        {
+          date: new Date(year, month, '30'),
+          price: 'RM 399',
+        },
+      ],
+    };
+  },
 }
 </script>
 
--- src/components/PriceCalendar.vue
+++ src/components/PriceCalendar.vue
@@ -1,5 +1,24 @@
 <template>
-  <VCalendar :attributes="attrs" />
+  <v-calendar :attributes="attrs">
+    <template v-slot:day-content="{ day, attributes }">
+      <div
+        class="flex flex-col h-full z-10 overflow-hidden"
+        v-for="attr in attributes"
+        :key="attr.key"
+      >
+        <span class="day-label text-sm day-with-price" >{{ day.day }}</span>
+        <div class="flex-grow overflow-y-auto overflow-x-auto">
+          <div
+            class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
+            style="font-size: 8px"
+            :class="attr.customData.class"
+          >
+            {{ attr.customData.price }}
+          </div>
+        </div>
+      </div>
+    </template>
+  </v-calendar>
 </template>
 
 <script>
@@ -9,50 +28,50 @@ export default {
   name: 'PriceCalendar',
   data() {
     const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return {
-      attrs: [
-        {
-          key: 'today',
+    const updatedPrices = this.prices.map((value) => {
+      if (value.date.getDate() === date.getDate()) {
+        return {
+          key: value.toString(),
+          ...value,
           highlight: {
             color: 'red',
             fillMode: 'solid',
-            // contentClass: 'italic',
           },
-          dates: new Date(year, month, day),
-        },
-        {
-          dates: { start: new Date(year, month, 14), end: new Date(year, month, 30) },
-          dot: {
-            color: 'red',
-            class: 'price-text-below-date',
-          },
-          popover: {
-            label: 'RM 775',
-            hideIndicator: true,
-          },
-        },
+        };
+      }
+
+      return {
+        key: value.toString(),
+        ...value,
+      };
+    });
+
+    return {
+      attrs: [
+        ...updatedPrices.map(priceInfo => ({
+          key: priceInfo.key,
+          highlight: priceInfo.highlight,
+          dates: priceInfo.date,
+          customData: priceInfo,
+        })),
       ],
     };
   },
   components: {
-    VCalendar
+    VCalendar,
   },
   props: {
-    msg: String
+    prices: Array,
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  .vc-container {
-    /* --text-sm: 18px; */
-  }
   .vc-day {
     margin: 2px;
+    width: 42px !important;
+    height: 42px !important;
   }
   .vc-day-content {
     width: 42px !important;
@@ -62,4 +81,14 @@ export default {
     width: 100% !important;
     height: 100% !important;
   }
+  .vc-highlights + div > .day-with-price {
+    color: #fff;
+  }
+  .vc-highlights + div > .day-with-price + div {
+    color: #fff;
+  }
+  .day-with-price {
+    color: #f00;
+  }
+
 </style>
